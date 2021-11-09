11c9563532c8f91f7a80b9e7e01ccc392899f1c6
alexanderlindhardt <alexanderlindhardt12@gmail.com>
2021-11-09 18:04:37 +0100

    Completed challenge 3.3

--- src/Challenge_3_3.java
+++ src/Challenge_3_3.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Random;
 public class Challenge_3_3 {
 
@@ -9,22 +8,16 @@ public class Challenge_3_3 {
         int firstDigit = rand.nextInt(8);
         int secondDigit = rand.nextInt(8);
         int thirdDigit = rand.nextInt(8);
-
         phoneNumber += firstDigit + ""+ secondDigit + "" +thirdDigit;
         phoneNumber += "-";
-        int middleThreeDigits = rand.nextInt(100,743);
 
-        if (middleThreeDigits < 100 || middleThreeDigits > 742)
-            System.out.println(middleThreeDigits);
+        int middleThreeDigits = rand.nextInt(100,743);
         phoneNumber += middleThreeDigits;
         phoneNumber += "-";
+
         int lastThreeDigits = rand.nextInt(1000,10000);
-        if (lastThreeDigits < 1000 || lastThreeDigits > 9999)
-            System.out.println(lastThreeDigits);
         phoneNumber += lastThreeDigits;
+
         System.out.println(phoneNumber);
-
-
-
     }
 }
